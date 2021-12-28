4062b6126820aa8ca8c25a5f9792df4dd60ca6f8
Viktor <f49411151@gmail.com>
2021-12-28 14:18:46 +0300

    Константы в JS

--- src/script.js
+++ src/script.js
@@ -1,46 +1,13 @@
 'use strict'
 
-/* Константы */
-// по соглашению константы пишутся в caps lock
-const CONSTANTA = 10;  
+/* Константы в JS.
+Идентификатор константы не может быть переназначен. Нельзя поменять ее 
+присваиванием. Но можно изменить содержимое объекта на который указывает константа */
+// по соглашению константы пишутся в caps lock 
+// const CONSTANTA = 10;  
 
-const arr = []
-arr = 100 // error
+// const arr = []
+// arr = 100 // error
 
-arr [0] = 10 // ok
-arr.push (100) // ok
-
-/* Стрелочные функции */
-function sum (arg1, arg2) {
-return arg1 + arg2
-}
-// Будет равна стрелочной функции
-let sum = (arg1, arg2) => arg1 + arg2 // return суммы
-
-
-
-function mathOperation (arg1, arg2, op) {
-if (op === 's') {
-    return sum (arg1, arg2)
-}
-}
-// Будет равна стрелочной функции
-let mathOperation = (arg1, arg2, op) => {
-if (op === 's') {
-    return sum (arg1, arg2)
-}
-}
-
-window.addEventListener ('load', function() {
-
-})
-// Будет равна стрелочной функции
-window.addEventListener ('load', () => {
-})
-
-console.log(mathOperation (10, 55, 's'))
-
-
-let str = 'I like JS'
-let arr = [...str]
-arr.forEach = [e => console.log (e)] // форма записи стрелочной функции
+// arr [0] = 10 // ok
+// arr.push (100) // ok
