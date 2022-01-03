15f8280bcc21ac3fcc8117a0d98488d138635c88
Viktor <f49411151@gmail.com>
2022-01-03 12:46:06 +0300

    Формат передачи данных JSON

--- src/script.js
+++ src/script.js
@@ -1,39 +1,38 @@
 'use strict'
 
-/* Методы не требующие внешнего вызова (инкапсулированные)
-запускаемые при обращении к классу (созданию нового экземпляра 
-объекта на основе конструктора класса). Пример */
-    class Catalog {
-        constructor () {
-            this.products = []
-            /* 2 Прописываем запуск этого метода прямо из конструктора по умолчанию
-            * метод выполняемый внутри конструктора (других методов по факту) называется инкапсулированный
-            * и по соглашению и его название начинается с нижнего подчеркивания чтобы не перепутать.
-            * Методы вызываемые снаружи называются общими (неинкапсулированные) */
-            this._init()
-        }
-    /* 1 Создаем метод */
-        _init() {
-            this._fetchProducts()
-            this._render()
-        }
-        _fetchProducts() {
-            for (let i = 0; i < image.length; i++) {
-                this.products.push(new Product(ids[i], items[i], prices[i]))
-            }
-    
-        }
-        _render() {
-            const block = document.querySelector('.products')
-    /* Можно использовать forEach, но для разнообразия используем for of он полноценно заменяет forEach */
-            let htmlString = ''
-            for (let item of this.products) {
-                htmlString += item.render()
-            }
-            block.innerHTML = htmlString
-        }
+/* Способы передачи данных
+1) JSON (JavaScript Object Notation) похож на объект JS
+2) XML расширенный язык разметки, похож на HTML 
+3) HTML/text - разметка или текст
+4) Бинарные данные (0. 1) */
+
+/* Синтаксис JSON
+Название полей в двойных кавычках 
+Строковые данные в двойных кавычках 
+Число и boolean без кавычек */
+jsonData = [
+    {
+      "id_product": 123,  
+      "product_name": "Ноутбук",
+      "price": 45600,
+      "avaliable": true
+    },
+    {
+      "id_product": 456,
+      "product_name": "Мышка",
+      "price": 1000,
+      "avaliable": false
     }
-    
-    /* При создании нового объекта на основе конструктора с инкапсулированным методом
-    *  не нужно запускать метод извне */
-    let catalog = new Catalog() // render сработает сразу при создании нового экземпляра объекта
+  ]
+  /* В таком формате данные можно записывать в отдельный репозиторий на github.com
+  Для получения ссылки доступа на отправку кода JSON нужно нажать на кнопку Raw */
+let jsonObj = '{"id_product": 123, "product_name": "Ноутбук", "price": 45600}' // правильный формат
+
+/* Метод JSON.parse() преобразовывает JSON в объект */
+let obj = JSON.parse(jsonObj)
+
+/* Метод JSON.stringify() преобразовывает объект в JSON */
+let againJSON = JSON.stringify(obj)
+// пример 2
+let arr = [{a: 1}, {a: 100, b: [1, 2, 'qwert']}]
+let jsonArrObj = JSON.stringify(arr) 
